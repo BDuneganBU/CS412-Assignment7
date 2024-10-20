3003ac1d165063c6df16df9d7995ee306b381489
BDuneganBU <bdunegan@bu.edu>
2024-10-20 18:35:59 -0400

    Update settings.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -85,19 +85,6 @@ WSGI_APPLICATION = 'cs412.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     "NAME": 'railway',
-    #     "USER": 'postgres',
-    #     "PASSWORD": 'vBhRNmPoJETVqbMbFjtYbvAEuphnBHal',
-    #     "HOST": 'autorack.proxy.rlwy.net',
-    #     "PORT": '22746',
-    #     #os.environ.get('DB_PORT'),
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         "NAME": os.environ.get('DB_NAME'),
